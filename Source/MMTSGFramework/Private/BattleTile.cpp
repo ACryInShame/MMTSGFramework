c343d84076f6162aba071c7266d24de0c18744ac
Louis D. <ciller7765@yahoo.com>
2026-06-09 00:58:21 -0400

    Battle Tile Initalization
    Create the basics for Battle Tile
    - Enum for type of Tile ( Square, Hex, Flat top Hex)
    - Static meshes that can be assigned Via the editor
    
    updated main map with test tiles and a Grid Manager to test spawning a Tile
    Had to overwrite UE constructor to get the static mesh switch to work properly.

--- Source/MMTSGFramework/Private/BattleTile.cpp
+++ Source/MMTSGFramework/Private/BattleTile.cpp
@@ -7,8 +7,12 @@
 ABattleTile::ABattleTile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+    TileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TileMesh"));
+
+    //update mesh and then set root to updated mesh
+	RootComponent = TileMesh;
 }
 
 // Called when the game starts or when spawned
@@ -25,3 +29,29 @@ void ABattleTile::Tick(float DeltaTime)
 
 }
 
+//override of UE constructor
+void ABattleTile::OnConstruction(const FTransform& Transform)
+{
+    Super::OnConstruction(Transform);
+
+    UpdateStaticMesh();
+}
+
+//updated the tile mesh based on the type of tile being spawned
+void ABattleTile::UpdateStaticMesh()
+{
+    switch (TileShape)
+    {
+    case TileShapes::Square:
+        TileMesh->SetStaticMesh(SquareMesh);
+        break;
+
+    case TileShapes::HexFlatTop:
+        TileMesh->SetStaticMesh(FlatHexMesh);
+        break;
+
+    case TileShapes::HexPointedTop:
+        TileMesh->SetStaticMesh(PointedHexMesh);
+        break;
+    }
+}
--- Source/MMTSGFramework/Public/BattleTile.h
+++ Source/MMTSGFramework/Public/BattleTile.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Actor.h"
 #include "BattleTile.generated.h"
 
+//Enum for Tile Type
+UENUM(BlueprintType)
+enum class TileShapes : uint8
+{
+	Square,
+	HexFlatTop,
+	HexPointedTop
+};
+
 UCLASS()
 class MMTSGFRAMEWORK_API ABattleTile : public AActor
 {
@@ -14,13 +23,38 @@ class MMTSGFRAMEWORK_API ABattleTile : public AActor
 public:	
 	// Sets default values for this actor's properties
 	ABattleTile();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+	void UpdateStaticMesh();
+
+	//override of UE constructor
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn = "true"), Category = "Components")
+	TileShapes TileShape;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* TileMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+	int32 GridX = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+	int32 GridY = 0;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TileTypes")
+	UStaticMesh* SquareMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TileTypes")
+	UStaticMesh* FlatHexMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TileTypes")
+	UStaticMesh* PointedHexMesh;
 
 };
